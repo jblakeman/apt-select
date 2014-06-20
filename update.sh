872af5aea1b4b35d4ce42792e9afe714186a8336
jblakeman <blakeman.john@gmail.com>
2014-06-19 20:13:06 -0700

    check for backup copy

--- update.sh
+++ update.sh
@@ -1,14 +1,41 @@
 #!/bin/sh
 
 apt="/etc/apt"
+file="sources.list"
+apt_file=${apt}/${file}
+backup=${apt_file}.backup
+
+update (){
+    sudo mv $apt_file $backup  && \
+    sudo mv $file $apt_file && \
+    echo "apt has been updated"
+    exit 0
+}
+
+isBackup (){
+    local query options answer
+    query="Copied original file '$backup' exists.\nOverwrite?"
+    options="Options:\n[y] for yes\n[n] for no "
+    if [ -z "$1" ]; then
+        echo -en "$query\n$options"
+    else
+        echo -en "$options"
+    fi
+    read answer
+    case $answer in 
+        y) update;;
+        n) echo "apt not updated";;
+        *) isBackup 1;;
+    esac
+}
+
 if [ "$PWD" = "$apt" ]; then
-    echo "Please run the update from a directory other than '$apt'"
+    echo "Please run the update from a directory other than '%$apt'"
     exit 1
 else
-    file="sources.list"
     if [ -f "$file" ]; then
-        sudo mv ${apt}/$file ${apt}/${file}.backup && \
-        sudo mv $file ${apt}/$file
+        backup=${apt}/${file}.backup
+        [ -f "$backup" ] && isBackup || update
     else
         echo "File '$file' must exist in the current directory"
         exit 1
