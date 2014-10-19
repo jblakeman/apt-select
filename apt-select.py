04324ae254027e50aa9227c9795b3dddc5532ca7
John Blakeman <blakeman.john@gmail.com>
2014-10-18 22:33:41 -0700

    removed '--auto' flag; added '--list' and '--choose' flags

--- apt-select.py
+++ apt-select.py
@@ -15,8 +15,11 @@ from mirrors import RoundTrip, Data, statuses
 
 parser = ArgumentParser(description="Find the fastest Ubuntu mirrors",
                         formatter_class=RawTextHelpFormatter)
-parser.add_argument('-a', '--auto', action='store_true',
-                    help="choose the best mirror", default=False)
+parser.add_argument('-t', '--top-number', nargs=1, type=int,
+                    help=(
+                        'Specify number of mirrors to return\n'
+                        '   default=1\n'
+                    ), default=1, metavar='NUMBER')
 
 status_args = [
     x[0].lower() + x[1:] for x in [
@@ -28,7 +31,7 @@ status_args.reverse()
 parser.add_argument('-m', '--min-status', nargs=1,
                     choices=status_args,
                     help=(
-                        'return mirrors with minimum status\n'
+                        'Return mirrors with minimum status\n'
                         'choices:\n'
                         '   %(up)s\n'
                         '   %(day)s\n'
@@ -45,18 +48,44 @@ parser.add_argument('-m', '--min-status', nargs=1,
                     ),
                     default=status_args[0], metavar='STATUS')
 
+parser.add_argument('-l', '--list', dest='list_only', action='store_true',
+                    help=(
+                        "Print list of mirrors only.\n"
+                        "Don't generate file."
+                    ),
+                    default=False)
+
+parser.add_argument('-c', '--choose', action='store_true',
+                    help=(
+                        "Choose mirror from a list.\n"
+                        "Requires -t, --top-num option\n"
+                        "with an argument > 2."
+                    ), default=False)
+
 args = parser.parse_args()
-flag_status = args.min_status
 
 # argparse returns list type for only choice arguments, not default
-if type(flag_status) is list:
-    flag_status = flag_status[0]
+def indexZero(flag):
+    if type(flag) is list:
+        return flag[0]
+    else:
+        return flag
+
+flag_number = indexZero(args.top_number)
+flag_status = indexZero(args.min_status)
 
 flag_status = flag_status.replace('-', ' ')
 if flag_status != 'unknown':
     flag_status = flag_status[0].upper() + flag_status[1:]
 
-flag_auto = args.auto
+flag_list = args.list_only
+flag_choose = args.choose
+
+
+if flag_choose and (not flag_number or flag_number < 2):
+    parser.print_usage()
+    print("-c, --choose option requires -t, --top-number NUMBER where NUMBER is greater than 1")
+    exit(1)
 
 def notUbuntu():
     print("Not an Ubuntu OS")
@@ -104,7 +133,6 @@ if hardware == 'x86_64':
 else:
     hardware = 'i386'
 
-top_num = 5
 ranks = sorted(avg_rtts, key=avg_rtts.__getitem__)
 info = []
 print("Looking up status information")
@@ -115,25 +143,28 @@ for rank in ranks:
         info.append(data)
 
     info_size = len(info)
-    if info_size == top_num:
+    if info_size == flag_number:
         break
 
 if info_size == 0:
     print("Unable to find alternative mirror status(es)")
     exit(1)
 elif info_size == 1:
-    print("Alternative mirror found:")
+    header = "\nTop mirror:\n"
 else:
-    print("\nTop %d mirrors:\n" % info_size)
+    header = "\nTop %d mirrors:\n" % info_size
+
+print(header)
 
 directory = '/etc/apt/'
 apt_file = 'sources.list'
 found = None
-deb = ('deb', 'deb-src')
-proto = ('http://', 'ftp://')
 with open('%s' % directory + apt_file, 'r') as f:
     lines = f.readlines()
-    def confirmMirror(url, deb, proto):
+    def confirmMirror(url):
+        deb = ('deb', 'deb-src')
+        proto = ('http://', 'ftp://')
+
         if (url and (url[0] in deb) and
                 (proto[0] in url[1] or
                  proto[1] in url[1])):
@@ -144,13 +175,13 @@ with open('%s' % directory + apt_file, 'r') as f:
     for line in lines:
         fields = line.split()
         if not found:
-            if (confirmMirror(fields, deb, proto) and
+            if (confirmMirror(fields) and
                     (release[1] in fields[2])):
                 repo = [fields[1]]
                 found = True
                 continue
         else:
-            if (confirmMirror(fields, deb, proto) and
+            if (confirmMirror(fields) and
                 (fields[2] == '%s-security' % (release[1]))):
                 repo += [fields[1]]
                 break
@@ -175,81 +206,82 @@ try:
 except NameError:
     pass
 
-def ask(query, default):
-    global input, flag_auto
-    if flag_auto:
-        return default
-
+def ask(query):
+    global input
     answer = input(query)
     return answer
 
-query = "Choose a mirror (1 - %d)\n'q' to quit " % info_size
-key = ask(query, '1')
+if flag_choose:
+    query = "Choose a mirror (1 - %d)\n'q' to quit " % info_size
+    key = ask(query)
 
-while True:
-    match = search(r'[1-5]', key)
-    if match and (len(key) == 1):
-        key = int(key)
-        break
-    elif key == 'q':
-        exit()
-    else:
-        query = "Invalid entry "
-        key = ask(query, '1')
+    while True:
+        match = search(r'[1-5]', key)
+        if match and (len(key) == 1):
+            key = int(key)
+            break
+        elif key == 'q':
+            exit()
+        else:
+            query = "Invalid entry "
+            key = ask(query)
 
-key = key - 1
-if current_key == key:
-    print("The mirror you selected is the currently used mirror.\n"
-           "There is nothing to be done.")
+    key = key - 1
+    if current_key == key:
+        print("The mirror you selected is the currently used mirror.\n"
+               "There is nothing to be done.")
     exit(0)
 
-mirror = info[key][0]
+    mirror = info[key][0]
+
+# Switch mirror from resolvable url back to full http/ftp path
 for m in archives.splitlines():
     if mirror in m:
         mirror = m
         break
 
-lines = ''.join(lines)
-for r in repo:
-    lines = lines.replace(r, mirror)
+if not flag_list:
+    lines = ''.join(lines)
+    for r in repo:
+        lines = lines.replace(r, mirror)
 
-def yesOrNo():
-    y = 'yes'
-    n = 'no'
-    query = ''
-    options = "Please enter '%s' or '%s': " % (y,n)
-    while True:
-        answer = ask(query, 'yes')
-        if answer == y:
-            break
-        elif answer == n:
-            exit(0)
+    def yesOrNo():
+        y = 'yes'
+        n = 'no'
+        query = ''
+        options = "Please enter '%s' or '%s': " % (y,n)
+        while True:
+            answer = ask(query, 'yes')
+            if answer == y:
+                break
+            elif answer == n:
+                exit(0)
+            else:
+                query = options
+
+    wd = getcwd()
+    if wd == directory[0:-1]:
+        query = (
+            "'%(dir)s' is the current directory.\n"
+            "Generating a new '%(apt)s' file will "
+            "overwrite the current file.\n"
+            "You should copy or backup '%(apt)s' before replacing it.\n"
+            "Continue?\n[yes|no] " %
+            {'dir': directory, 'apt': apt_file}
+        )
+        yesOrNo()
+
+    try:
+        with open(apt_file, 'w') as f:
+            f.write(lines)
+    except IOError as err:
+        if err.strerror == 'Permission denied':
+            print(("%s\nYou do not own %s\n"
+                   "Please run the script from a directory you own." %
+                   (err, wd)))
+            exit(1)
         else:
-            query = options
-
-wd = getcwd()
-if wd == directory[0:-1]:
-    query = (
-        "'%(dir)s' is the current directory.\n"
-        "Generating a new '%(apt)s' file will "
-        "overwrite the current file.\n"
-        "You should copy or backup '%(apt)s' before replacing it.\n"
-        "Continue?\n[yes|no] " %
-        {'dir': directory, 'apt': apt_file}
-    )
-    yesOrNo()
-
-try:
-    with open(apt_file, 'w') as f:
-        f.write(lines)
-except IOError as err:
-    if err.strerror == 'Permission denied':
-        print(("%s\nYou do not own %s\n"
-               "Please run the script from a directory you own." %
-               (err, wd)))
-        exit(1)
-    else:
-        print(err)
-        exit(1)
+            print(err)
+            exit(1)
 
 exit(0)
