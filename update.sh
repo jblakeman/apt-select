97b6453bfbc879ee9b5a2de933ea9bf62617bccf
Johan Westin <johan.m.westin@gmail.com>
2024-03-03 10:51:47 +0100

    Apply shellcheck

--- update.sh
+++ update.sh
@@ -1,83 +1,87 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-apt=/etc/apt
-file=sources.list
-apt_file=${apt}/${file}
-backup=${apt_file}.backup
+declare -r APT_DIR_PATH='/etc/apt'
+declare -r APT_SOURCES_FILE_NAME='sources.list'
+declare -r APT_SOURCES_FILE_PATH="${APT_DIR_PATH}/${APT_SOURCES_FILE_NAME}"
+declare -r APT_SOURCES_BACKUP_FILE_PATH="${APT_SOURCES_FILE_PATH}.backup"
 
-if [ $EUID -ne 0 ]; then
-    echo "$0 needs sudoer priveleges to modify ${apt_file}"
+# shellcheck disable=SC2046
+if [ $(id -u) -ne 0 ]; then
+    echo "${0} needs root privilege to modify ${APT_SOURCES_FILE_PATH}"
     echo "please run script as super user (root)"
     exit 1
 fi
 
-updateApt (){
-    mv $file $apt_file &&
-    echo "apt has been updated"
+update_apt (){
+    mv "${APT_SOURCES_FILE_NAME}" "${APT_SOURCES_FILE_PATH}" &&
+    echo "APT_DIR_PATH has been updated"
 }
 
-updateBackup (){
-    mv $apt_file $backup &&
-    echo "$apt_file backed up to $backup"
-    updateApt
+update_backup (){
+    mv "${APT_SOURCES_FILE_PATH}" "${APT_SOURCES_BACKUP_FILE_PATH}" &&
+    echo "${APT_SOURCES_FILE_PATH} backed up to ${APT_SOURCES_BACKUP_FILE_PATH}"
+    update_apt
 }
 
 examine (){
-    less $1 2>/dev/null
-    isBackup
-    break
+    less "${1}" 2>/dev/null
+    is_backup
 }
 
-isBackup (){
+is_backup (){
     local query options opt
-    query="Backup file $backup already exists.\n"
+    query="Backup APT_SOURCES_FILE_NAME ${APT_SOURCES_BACKUP_FILE_PATH} already exists.\n"
     query+="Choose one of the following options:"
-    echo -e "$query"
+    echo -e "${query}"
     options=(
         "Replace backup and update apt"
         "Update apt without backing up"
-        "Examine $backup"
-        "Examine $apt_file"
-        "Examine $PWD/$file"
+        "Examine ${APT_SOURCES_BACKUP_FILE_PATH}"
+        "Examine ${APT_SOURCES_FILE_PATH}"
+        "Examine ${PWD}/${APT_SOURCES_FILE_NAME}"
         "Quit"
     )
     select opt in "${options[@]}"; do
-        case $opt in
+        case ${opt} in
             "${options[0]}")
-                updateBackup
+                update_backup
                 break
                 ;;
             "${options[1]}")
-                updateApt
+                update_apt
                 break
                 ;;
             "${options[2]}")
-                examine $backup
+                examine "${APT_SOURCES_BACKUP_FILE_PATH}"
                 ;;
             "${options[3]}")
-                examine $apt_file
+                examine "${APT_SOURCES_FILE_PATH}"
                 ;;
             "${options[4]}")
-                examine $file
+                examine "${APT_SOURCES_FILE_PATH}"
                 ;;
             "${options[5]}")
                 break
                 ;;
-            *) 
+            *)
                 echo invalid option
                 ;;
         esac
     done
 }
 
-if [ "$PWD" = "$apt" ]; then
-    echo "Please run the update from a directory other than $apt"
+if [ "${PWD}" = "${APT_DIR_PATH}" ]; then
+    echo "Please run the update from a directory other than ${APT_DIR_PATH}"
     exit 1
 else
-    if [ -f "$file" ]; then
-        [ -f "$backup" ] && isBackup || updateBackup
+    if [ -f "${APT_SOURCES_FILE_NAME}" ]; then
+        if [ -f "${APT_SOURCES_BACKUP_FILE_PATH}" ]; then
+          is_backup
+        else
+          update_backup
+        fi
     else
-        echo "$file must exist in the working directory"
+        echo "${APT_SOURCES_FILE_NAME} must exist in the working directory"
         exit 1
     fi
 fi
